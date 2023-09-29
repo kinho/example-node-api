e8687922c629f87cc5d3e44d3eb763c53ed769a5
kinho <kiinho@gmail.com>
2023-09-28 22:09:29 -0300

    feat: news register and list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import auth from './auth.js'
 import user from './user.js'
+import news from './news.js'
 
 const routes = (app) => {
   app.use('/',     auth)
   app.use('/user', user)
+  app.use('/news', news)
 }
 
 export default routes
---
+++ routes/news.js
@@ -0,0 +1,35 @@
+import Router from 'express-promise-router'
+
+import { query } from '../db/index.js'
+import { authMiddleware } from './middleware.js'
+
+const router = new Router()
+export default router
+
+router.get('/', authMiddleware, async (_, res, next) => {
+  try {
+    const select = 'SELECT news.*, users.username FROM news LEFT JOIN users ON users.id = news.author_id ORDER BY news.id DESC LIMIT 50'
+    const { rows } = await query(select)
+    res.send(rows)
+
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.post('/', authMiddleware, async (req, res, next) => {
+  try {
+    const { title, description } = req.body
+
+    const insert = 'INSERT INTO news(title, description, author_id) VALUES($1, $2, $3)'
+    const values = [title, description, req.user_id]
+
+    const { rowCount } = await query(insert, values)
+
+    res.status(rowCount > 0 ? 200 : 500)
+    return res.end()
+
+  } catch (e) {
+    next(e)
+  }
+})
