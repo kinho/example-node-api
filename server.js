a070d29c778f47c777a764944ae95c732fd14871
kinho <kiinho@gmail.com>
2023-09-29 21:21:11 -0300

    fix: add cors on start server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { config } from 'dotenv'
 
 import routes from './src/routes/index.js'
@@ -7,6 +8,7 @@ config()
 const { NODE_DOCKER_PORT } = process.env
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 routes(app)
 
